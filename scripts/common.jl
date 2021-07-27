40e879ce9aa1ac5a9ee71d3945a55dddda482339
fpacaud <francoispacaud8@gmail.com>
2021-07-27 11:36:56 +0200

    update scripts

--- scripts/common.jl
+++ scripts/common.jl
@@ -1,22 +1,40 @@
 
 using ExaPF
 using LinearAlgebra
+using SuiteSparse
 using KernelAbstractions
 using CUDA
-using CUDA.CUSPARSE
 using BlockPowerFlow
 
-using CUDAKernels
+const CUDA_DEVICE = 1
+# Remove iterative refinement in UMFPACK
+SuiteSparse.UMFPACK.umf_ctrl[8] = 0.0
 
-function build_batch_auglag(datafile, device, nbatch; ρ=0.1, scale=false)
-    pf_solver = NewtonRaphson(; tol=1e-10)
+function build_problem(datafile; scale=true, ρ=0.1, pf_tol=1e-10)
     nlp = ExaOpt.ReducedSpaceEvaluator(
-        datafile; device=device, nbatch_hessian=nbatch, powerflow_solver=pf_solver,
+        datafile;
+        powerflow_solver=ExaPF.NewtonRaphson(tol=pf_tol),
     )
-    bdg = ExaOpt.BridgeDeviceEvaluator(nlp, CuArray{Float64, 1}, CuArray{Float64, 2})
-    slk = ExaOpt.SlackEvaluator(bdg)
+    slk = ExaOpt.SlackEvaluator(nlp)
     x0 = ExaOpt.initial(slk)
-    aug = ExaOpt.AugLagEvaluator(slk, x0; c₀=ρ, scale=scale)
-    return aug
+    return ExaOpt.AugLagEvaluator(slk, x0; c₀=ρ, scale=scale)
+end
+
+if has_cuda_gpu()
+    using CUDAKernels
+    using CUDA.CUSPARSE
+    device!(CUDA_DEVICE)
+
+    function build_batch_problem(datafile, nbatch; ρ=0.1, scale=false)
+        pf_solver = NewtonRaphson(; tol=1e-10)
+        nlp = ExaOpt.ReducedSpaceEvaluator(
+            datafile; device=CUDADevice(), nbatch_hessian=nbatch, powerflow_solver=pf_solver,
+        )
+        bdg = ExaOpt.BridgeDeviceEvaluator(nlp, CuArray{Float64, 1}, CuArray{Float64, 2})
+        slk = ExaOpt.SlackEvaluator(bdg)
+        x0 = ExaOpt.initial(slk)
+        aug = ExaOpt.AugLagEvaluator(slk, x0; c₀=ρ, scale=scale)
+        return aug
+    end
 end
 
--- scripts/ipopt/main.jl
+++ scripts/ipopt/main.jl
@@ -8,65 +8,49 @@ using Ipopt
 using SuiteSparse
 
 using MathOptInterface
-
 const MOI = MathOptInterface
 
-# Remove iterative refinement in UMFPACK
-SuiteSparse.UMFPACK.umf_ctrl[8] = 0.0
+include(joinpath(dirname(@__FILE__), "..", "common.jl"))
+include(joinpath(dirname(@__FILE__), "..", "problems.jl"))
 
-function subproblem(nlp; ρ=0.1, scale=true)
-    x0 = ExaPF.initial(nlp)
-    aug = @time ExaPF.AugLagEvaluator(nlp, x0; c₀=ρ, scale=scale)
+function ipopt_optimizer()
     solver = Ipopt.Optimizer()
-    MOI.set(solver, MOI.RawParameter("print_level"), 5)
+    # Default parameters
+    MOI.set(solver, MOI.RawParameter("print_level"), 0)
     MOI.set(solver, MOI.RawParameter("tol"), 1e-4)
-    return ExaPF.optimize!(solver, aug, x0)
+    return solver
+end
+
+function ipopt_subproblem(aug)
+    optimizer = ipopt_optimizer()
+    MOI.set(solver, MOI.RawParameter("print_level"), 5)
+    solution = @time ExaOpt.optimize!(optimizer, aug)
+    MOI.empty!(optimizer)
 end
 
 # Augmented Lagrangian method
-function auglag(nlp, x0; iter_max=200, itout_max=1)
+function solve_auglag(aug; max_iter=20, penalty=0.1, rate=10.0)
     # Init a penalty evaluator with initial penalty c₀
     algo = ExaOpt.AugLagSolver(;
-        max_iter=30,
-        max_inner_iter=iter_max,
-        scaling=true,
+        max_iter=max_iter,
+        max_inner_iter=100,
         α0=1.0,
-        ρ0=1e1,
+        ρ0=penalty,
+        rate=rate,
         ωtol=1e-3,
         verbose=1,
         inner_algo=:MOI,
         ε_dual=1e-2,
         ε_primal=1e-3,
-        lsq_lambda=false,
     )
-    c0 = algo.ρ0
+    aug.ρ = penalty
+    x0 = ExaOpt.initial(aug)
 
-    aug = @time ExaOpt.AugLagEvaluator(nlp, x0; c₀=c0, scale=true)
-    opt = () -> Ipopt.Optimizer(print_level=0, tol=1e-4)
-    solution = @time ExaOpt.optimize!(algo, aug, x0; moi_optimizer=opt)
+    solution = @time ExaOpt.optimize!(algo, aug, x0; moi_optimizer=ipopt_optimizer)
     return aug, solution
 end
 
-datafile = joinpath(dirname(@__FILE__), "..", "..", "..", "ExaPF.jl", "data", "case9.m")
-datafile = joinpath(dirname(@__FILE__), "..", "..", "..", "ExaPF.jl", "data", "case57.m")
-# datafile = joinpath(dirname(@__FILE__), "..","..", "..", "ExaPF.jl", "data", "case300.m")
-datafile = joinpath(dirname(@__FILE__), "..","..", "..", "ExaPF.jl", "data", "case118.m")
-# datafile = "../pglib-opf/pglib_opf_case57_ieee.m"
-# datafile = "../../pglib-opf/pglib_opf_case118_ieee.m"
-# datafile = "../../pglib-opf/pglib_opf_case1354_pegase.m"
-# datafile = "../pglib-opf/pglib_opf_case9241_pegase.m"
+datafile = PROBLEMS["case300"]
+aug = build_problem(datafile)
+solution = @time solve_auglag(aug)
 
-constraints = Function[
-    ExaPF.voltage_magnitude_constraints,
-    ExaPF.active_power_constraints,
-    ExaPF.reactive_power_constraints,
-    # ExaPF.flow_constraints
-]
-
-pf_solver = ExaPF.NewtonRaphson(tol=1e-10)
-nlp = ExaOpt.ReducedSpaceEvaluator(datafile; constraints=constraints, powerflow_solver=pf_solver)
-slk = ExaOpt.SlackEvaluator(nlp)
-# u0 = fsol.minimizer
-# fsol = @time ExaOpt.tron_solve(ExaPF.FeasibilityEvaluator(slk), u0)
-# aug, solution = auglag(slk, u0; itout_max=20, iter_max=2000)
-sol = auglag(slk, ExaOpt.initial(slk))
