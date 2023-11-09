b5418416ba38896cc57d1c8d934610e8ec0a144b
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-11-09 01:37:42 -0600

    Use Block-GMRES in Argos.jl

--- src/krylov.jl
+++ src/krylov.jl
@@ -1,13 +1,13 @@
-
-struct KrylovWrapper{T, VT, SMT, K}
+struct KrylovWrapper{T, SMT, K1, K2}
     Gx::SMT
-    solver::K
+    solver1::K1
+    solver2::K2
 end
 
-function KrylovWrapper{T, VT}(Gx) where {T, VT}
-    nx = size(Gx, 1)
-    solver = Krylov.BicgstabSolver(nx, nx, VT)
-    return KrylovWrapper{T, VT, typeof(Gx), typeof(solver)}(Gx, solver)
+function KrylovWrapper{T, VT, MT}(Gx, n, p) where {T, VT, MT}
+    solver1 = ExaPF.BlockGmresSolver(n, n, p, 1, VT, MT)
+    solver2 = Krylov.GmresSolver(n, n, 1, VT)
+    return KrylovWrapper{T, typeof(Gx), typeof(solver1), typeof(solver2)}(Gx, solver1, solver2)
 end
 
 Base.size(kw::KrylovWrapper) = size(kw.Gx)
@@ -16,36 +16,27 @@ Base.size(kw::KrylovWrapper, dim::Integer) = size(kw.Gx, dim)
 LinearAlgebra.adjoint(kw::KrylovWrapper{T}) where T = LinearAlgebra.Adjoint{T, typeof(kw)}(kw)
 
 function LinearAlgebra.ldiv!(kw::KrylovWrapper{T}, x::AbstractVector{T}) where T
-    Krylov.solve!(kw.solver, kw.Gx, x; atol=1e-10, rtol=1e-10)
-    copyto!(x, kw.solver.x)
+    Krylov.solve!(kw.solver2, kw.Gx, x; verbose=1, atol=1e-8, rtol=0.0)
+    copyto!(x, kw.solver2.x)
     return x
 end
 
 function LinearAlgebra.ldiv!(akw::LinearAlgebra.Adjoint{T, KW}, x::AbstractVector{T}) where {T, KW<:KrylovWrapper{T}}
     kw = parent(akw)
-    Krylov.solve!(kw.solver, kw.Gx', x; atol=1e-10, rtol=1e-10)
-    copyto!(x, kw.solver.x)
+    Krylov.solve!(kw.solver2, kw.Gx', x; verbose=1, atol=1e-8, rtol=0.0)
+    copyto!(x, kw.solver2.x)
     return x
 end
 
 function LinearAlgebra.ldiv!(kw::KrylovWrapper{T}, X::AbstractMatrix{T}) where T
-    n, m = size(X)
-    for k in 1:m
-        x = view(X, :, k)
-        Krylov.solve!(kw.solver, kw.Gx, x; atol=1e-10, rtol=1e-10)
-        copyto!(x, kw.solver.x)
-    end
+    ExaPF.block_gmres!(kw.solver1, kw.Gx, X; verbose=1, atol=1e-10, rtol=1e-10)
+    copyto!(X, kw.solver1.X)
     return X
 end
 
 function LinearAlgebra.ldiv!(akw::LinearAlgebra.Adjoint{T, KW}, X::AbstractMatrix{T}) where {T, KW<:KrylovWrapper{T}}
     kw = parent(akw)
-    n, m = size(X)
-    for k in 1:m
-        x = view(X, :, k)
-        Krylov.solve!(kw.solver, kw.Gx', x; atol=1e-10, rtol=1e-10)
-        copyto!(x, kw.solver.x)
-    end
+    ExaPF.block_gmres!(kw.solver1, kw.Gx', X; verbose=1, atol=1e-10, rtol=1e-10)
+    copyto!(X, kw.solver1.X)
     return X
 end
-
