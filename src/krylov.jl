acc7b59ec8e182eeba5f4d2a94e8d5691109ed81
Xx-Alexis-xX <alexis.montoison@etu.enseeiht.fr>
2023-12-01 14:54:20 -0500

    Setup for François

--- src/krylov.jl
+++ src/krylov.jl
@@ -21,27 +21,27 @@ Base.size(kw::KrylovWrapper, dim::Integer) = size(kw.Gx, dim)
 LinearAlgebra.adjoint(kw::KrylovWrapper{T}) where T = LinearAlgebra.Adjoint{T, typeof(kw)}(kw)
 
 function LinearAlgebra.ldiv!(kw::KrylovWrapper{T}, x::AbstractVector{T}) where T
-    Krylov.solve!(kw.solver2, kw.Gx, x; N=kw.preconditioner, verbose=0, atol=1e-8, rtol=0.0)
+    Krylov.solve!(kw.solver2, kw.Gx, x; N=kw.preconditioner, verbose=1, atol=1e-8, rtol=0.0)
     copyto!(x, kw.solver2.x)
     return x
 end
 
 function LinearAlgebra.ldiv!(akw::LinearAlgebra.Adjoint{T, KW}, x::AbstractVector{T}) where {T, KW<:KrylovWrapper{T}}
     kw = parent(akw)
-    Krylov.solve!(kw.solver2, kw.Gx', x; N=kw.preconditioner, verbose=0, atol=1e-8, rtol=0.0)
+    Krylov.solve!(kw.solver2, kw.Gx', x; N=kw.preconditioner, verbose=1, atol=1e-8, rtol=0.0)
     copyto!(x, kw.solver2.x)
     return x
 end
 
 function LinearAlgebra.ldiv!(kw::KrylovWrapper{T}, X::AbstractMatrix{T}) where T
-    ExaPF.block_gmres!(kw.solver1, kw.Gx, X; N=kw.preconditioner, verbose=0, atol=1e-8, rtol=0.0)
+    ExaPF.block_gmres!(kw.solver1, kw.Gx, X; N=kw.preconditioner, verbose=1, atol=1e-8, rtol=0.0)
     copyto!(X, kw.solver1.X)
     return X
 end
 
 function LinearAlgebra.ldiv!(akw::LinearAlgebra.Adjoint{T, KW}, X::AbstractMatrix{T}) where {T, KW<:KrylovWrapper{T}}
     kw = parent(akw)
-    ExaPF.block_gmres!(kw.solver1, kw.Gx', X; N=kw.preconditioner, verbose=0, atol=1e-8, rtol=0.0)
+    ExaPF.block_gmres!(kw.solver1, kw.Gx', X; N=kw.preconditioner, verbose=1, atol=1e-8, rtol=0.0)
     copyto!(X, kw.solver1.X)
     return X
 end
